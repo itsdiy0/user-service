2e5f8ded4cef2b45a3293e2a2a40205ea2d8ed48
itsdiy0 <diyo11jen@gmail.com>
2025-09-26 16:39:21 +0100

    feat: added routes to app + mongodb as lifespan

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, HTTPException, status
 from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import settings
+from api.routes import auth
+from core.database import connect_to_mongo, close_mongo_connection
+from contextlib import asynccontextmanager
 
 
-app = FastAPI(title="My FastAPI App", version="1.0.0")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    await connect_to_mongo()
+    yield
+    # Shutdown
+    await close_mongo_connection()
+
+app = FastAPI(title="My FastAPI App", version="1.0.0",lifespan=lifespan)
+
+app.include_router(auth.router, prefix="/api/auth", tags=["Authentication"])
 
 @app.get("/")
 async def root():
