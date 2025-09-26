bc3cf93e37055044195d3c5bf08337d53e659e8e
itsdiy0 <diyo11jen@gmail.com>
2025-09-26 16:23:49 +0100

    fix: replacing __modify_schema__ with __get_pydantic_json_schema_ as it wasn't compatible with pydantic v2

--- app/models/user.py
+++ app/models/user.py
@@ -15,7 +15,7 @@ class PyObjectId(ObjectId):
         return ObjectId(v)
     # for fastAPI docs
     @classmethod
-    def __modify_schema__(cls, field_schema):
+    def __get_pydantic_json_schema__(cls, field_schema):
         field_schema.update(type="string")
 
 class UserInDB(BaseModel):
