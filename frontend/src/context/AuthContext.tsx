4b18d287a09aa5fbdc5114c03de20823daa35f46
itsdiy0 <diyo11jen@gmail.com>
2025-09-30 14:00:45 +0100

    feat: a context api for managing token in the localStorage

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import type { ReactNode } from "react";
+
+interface AuthContextType {
+  token: string | null;
+  login: (jwt: string) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  token: null,
+  login: () => {},
+  logout: () => {},
+});
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token")
+  );
+
+  const login = (jwt: string) => {
+    localStorage.setItem("token", jwt);
+    setToken(jwt);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
