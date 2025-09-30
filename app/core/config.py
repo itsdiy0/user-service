7b0fb52453358f3582b1976a79fa7600837119a4
itsdiy0 <diyo11jen@gmail.com>
2025-09-30 12:27:26 +0100

    feat: added CORS settings to app middleware + replaced mongodb start and stop logic with on_event method of app

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    allowed_hosts: list[str] = ["*"]
     
     class Config:
         env_file = "../.env"
--- app/main.py
+++ app/main.py
@@ -3,18 +3,26 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from core.config import settings
 from api.routes import auth
 from core.database import connect_to_mongo, close_mongo_connection
-from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup
+app = FastAPI(title="User Service API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_hosts,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def startup_event():
     await connect_to_mongo()
-    yield
-    # Shutdown
+
+@app.on_event("shutdown")
+async def shutdown_event():
     await close_mongo_connection()
-
-app = FastAPI(title="My FastAPI App", version="1.0.0",lifespan=lifespan)
-
+    
 app.include_router(auth.router, prefix="/api/auth", tags=["Authentication"])
 
 @app.get("/")
