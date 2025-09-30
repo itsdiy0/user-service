08112b8994ba32e038c68f19e6ce4a37c6ba07a5
itsdiy0 <diyo11jen@gmail.com>
2025-09-30 13:58:58 +0100

    feat: added response types to api calls

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,11 +1,23 @@
 export type registerType = {
     email: string;
     username: string;
-    full_name: string;
+    full_name?: string;
     password: string;
 }
 
 export type loginType = {
     email: string;
     password: string;
+}
+
+export type loginResponseType = {
+    access_token: string;
+    type: string;
+}
+
+export type registerResponseType = {
+    id: number;
+    email: string;
+    username: string;
+    full_name: string;
 }
